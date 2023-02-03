3e69c22733149d0e0ce96dff15cf8f21347ddfd5
zoubinnaba <emmanuelzoubinnaba@gmail.com>
2023-02-03 16:12:59 +0000

    remove previous code in order to design new calculator

--- js/index.js
+++ js/index.js
@@ -1,33 +1,4 @@
 
-const firstNumberInput = document.getElementById("first_number")
-const secondNumberInput = document.getElementById("second_number")
-const addEl = document.getElementById("add")
-const subsEl = document.getElementById("subs")
-const diviEl = document.getElementById("divi")
-const multEl = document.getElementById("mult")
-const resultEl = document.getElementById("result")
-
-
-
-const addition = () => {
-    const result = parseInt(firstNumberInput.value) + parseInt(secondNumberInput.value)
-    return  resultEl.textContent = result
-}
-
-const substraction = () => {
-    const result = parseInt(firstNumberInput.value) - (secondNumberInput.value)
-    return  resultEl.textContent = result
-}
-
-const division = () => {
-    const result = parseInt(firstNumberInput.value) / (secondNumberInput.value)
-    return  resultEl.textContent = parseFloat(result)
-}
-
-const multiply = () => {
-    const result = parseInt(firstNumberInput.value) * (secondNumberInput.value)
-    return  resultEl.textContent = result
-}
 
 
 addEl.addEventListener('click', addition)
